f787fc8a07b5997c248c96f62f7b5fbd59364124
ravitejalam <ravitejalam28@gmail.com>
2019-03-21 00:20:27 +0530

    hog phase 2

--- Python/ap.py
+++ Python/ap.py
@@ -1,22 +1,20 @@
 def solve(grid):
-    l=[[0,0,0],[0,0,0],[0,0,0]]
+    l = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     for i in range(5):
         for j in range(2):
-            if grid[i][j]>2:
+            if grid[i][j] > 2:
                 return "No"
     for i in range(5):
-        l[grid[i][0]][grid[i][1]]=1
-    if l==[[1,1,1],[0,1,0],[0,1,0]]:
+        l[grid[i][0]][grid[i][1]] = 1
+    if l == [[1, 1, 1], [0, 1, 0], [0, 1, 0]]:
         return "Yes"
-    elif l==[[0,1,0],[0,1,0],[1,1,1]]:
+    elif l == [[0, 1, 0], [0, 1, 0], [1, 1, 1]]:
         return "Yes"
-    elif l==[[1,0,0],[1,1,1],[1,0,0]]:
+    elif l == [[1, 0, 0], [1, 1, 1], [1, 0, 0]]:
         return "Yes"
-    elif l==[[0,0,1],[1,1,1],[0,0,1]]:
+    elif l == [[0, 0, 1], [1, 1, 1], [0, 0, 1]]:
         return "Yes"
     return "No"
-
-
 
 
 t = int(input().strip())
@@ -28,12 +26,11 @@ for t_itr in range(t):
 
     for _ in range(5):
         points.append(list(map(int, input().rstrip().split())))
-        xm=min(xm,points[_][0])
-        ym=min(ym,points[_][1])
+        xm = min(xm, points[_][0])
+        ym = min(ym, points[_][1])
     for _ in range(5):
-        points[_][0]-=xm
-        points[_][1]-=ym
-
+        points[_][0] -= xm
+        points[_][1] -= ym
 
     result = solve(points)
 
--- Python/sample.py
+++ Python/sample.py
@@ -1,8 +1,26 @@
-lines = []
-while True:
-    line = input()
-    if line:
-        lines.append(line)
-    else:
-        break
-print(lines)
+# array<struct<name:string,address:map<int,int>,credits:array<int>>>
+# [ {"name":"ravi",{23,06,43,11,28,78},[23,45,56,6,67]} ,6,43,11,28,78},[23,45,56,6,67]} ,{"name":"ravi",{23,06,43,11,28,78},[23,45,56,6,67]}]
+# array(   named_struct("name","ravi",map(23,06,43,11,28,78),array(23,45,56,6,67)), , )
+# select 'c1_val',named_struct('c2_a',array(cast (null as string)),'c2_c',cast (null as string)),array(named_struct('c3_a',cast (null as string) ,'c3_b',cast (null as string))) from z_dummy;
+#
+i=0
+stmt=""
+l=[]
+def create_structure(struct):
+    j=0
+    while j<len(struct):
+        if struct[j:j+6]=="array<":
+            c=1
+            for _ in struct[j+6:]:
+                if _==''
+
+
+
+
+
+
+
+structure = "array<struct<name:string,address:map<int,int>,credits:array<int>>>"
+data = '[{"name":"ravi",{23,06,43,11,28,78},[23,45,56,6,67]},{"name":"ravi",{23,06,43,11,28,78},[23,45,56,6,67]},{"name":"ravi",{23,06,43,11,28,78},[23,45,56,6,67]}]'
+print(create_insert(structure, data))
+
